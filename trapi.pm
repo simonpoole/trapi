6822998f0238d8aeb2e5b006394fadfeb01df4f6
Simon Poole <simon@poole.ch>
2017-03-25 12:26:56 +0100

    Add configuration option to ignore the timestamp when serving data

--- trapi.pm
+++ trapi.pm
@@ -13,12 +13,14 @@ use constant KEEPOPEN => 400;
 # bytes in node file before splitting
 use constant SPLIT => 4096;
 # directory trapi data is in
-use constant TRAPIDIR => '/trapi';
+use constant TRAPIDIR => './trapi';
 # directory where the indexes are kept (absolute or relitive to TRAPIDIR) end with / if not empty
-use constant DBDIR => '';
+use constant DBDIR => '/fast/home/simon/osm/src/trapi/trapi/';
 # regular expression of tags to ignore.  undef disables.
-use constant IGNORETAGS => '^(?:created_by$|tiger:|gnis:|source$|attribution$|import_uuid$|time$|AND[_:]|massgis:|open[gG]eo[dD][bB]:|converted_by$|KSJ2:|uploaded_by$|source_ref$|gns:)';
-
+#use constant IGNORETAGS => '^(?:created_by$|tiger:|gnis:|source$|attribution$|import_uuid$|time$|AND[_:]|massgis:|open[gG]eo[dD][bB]:|converted_by$|KSJ2:|uploaded_by$|source_ref$|gns:)';
+use constant IGNORETAGS => '';
+# ignore timestamp when serving data
+use constant IGNORETIMESTAMP => 1;
 # directory to save osc files in
 use constant TMPDIR => "/tmp/";
 # site to fetch osc.gz files from
